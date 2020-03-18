1c5e755aae8e27eb5d5a6d54c923d1c755dcceef
nunes-will <william-x7@hotmail.com>
2020-03-18 17:38:10 -0300

    Added a function to append a string to a file

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 const fs = require('fs')
 
-fs.writeFileSync('notes.docx', 'This file was created by node.js')
+fs.writeFileSync('notes.txt', 'This file was created by node.js')
+fs.appendFileSync('notes.txt', '\nI live in Brazil')
