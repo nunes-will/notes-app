3d2868bb8c401c77218f38dc13409837b7baaed3
nunes-will <william-x7@hotmail.com>
2020-03-18 18:52:35 -0300

    Add validator and chalk functions

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+const validator = require('validator')
+const chalk = require('chalk')
+
 const add = require('./utils.js')
 const fs = require('fs')
 const getNotes = require('./notes')
@@ -7,3 +10,5 @@ fs.appendFileSync('notes.txt', '\nI live in Brazil')
 
 console.log("require started first")
 console.log(add(1,3))
+
+console.log(chalk.black.bold.bgGreen(validator.isURL('https://print')))
